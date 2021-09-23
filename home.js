b9556ae885d7082dbc24ae0c360e37a152cc1756
Chandler Bottomley <bottomleychad@gmail.com>
2021-09-23 12:55:42 -0700

    update

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     loadDvds();
+    addDvd();
 });
 //http://dvd-library.us-east-1.elasticbeanstalk.com
 
@@ -29,7 +30,6 @@ function loadDvds() {
                     row += '<td>' + rating + '</td>';
                     row += '<td><a onlcick= "" >Edit</a> | <a onlcick= "">Delete</a></td>'     
                     row += '</tr>';
-                
                 contentRows.append(row);
             })
         },
@@ -40,4 +40,46 @@ function loadDvds() {
                 .text('Error calling web service. Please try again later.'));
         }
     }); 
+
+}
+
+function addDvd() {
+    $('#addDvdButton').click(function (event) {
+        //var haveValidationErrors = checkAndDisplayValidationErrors($('#addForm').find('input'));
+        
+        // if(haveValidationErrors) {
+        //     return false;
+        // }
+        $.ajax({
+           type: 'POST',
+           url: 'http://dvd-library.us-east-1.elasticbeanstalk.com/dvd',
+           data: JSON.stringify({
+                title: $('#addTitle').val(),
+                releaseYear: $('#addReleaseYear').val(),
+                director: $('#addDirector').val(),
+                rating: $('#addRating').val(),
+                notes: $('#addNotes').val(),
+           }),
+           headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+           },
+           'dataType': 'json',
+           success: function() {
+               $('#errorMessages').empty();
+               $('#add#addTitle').val('');
+               $('#addReleaseYear').val('');
+               $('#addDirector').val('');
+               $('#addRating').val('');
+               $('#addNotes').val('');
+               loadDvds();
+           },
+           error: function () {
+               $('#errorMessages')
+                .append($('<li>')
+                .attr({class: 'list-group-item list-group-item-danger'})
+                .text('Error calling web service. Please try again later.')); 
+           }
+        })
+    });
 }
